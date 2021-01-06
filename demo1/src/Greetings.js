d758712279604da133443dd13a104e5433d8b77d
adritasharma <adritasharma.developer@gmail.com>
2021-01-07 00:34:18 +0530

    header component created

---
+++ demo1/src/Greetings.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+export class Greetings extends Component {
+
+  render() {
+    return(
+      <Fragment>
+          <h2>Hello React!</h2>
+      </Fragment>
+    )
+  }
+
+}
